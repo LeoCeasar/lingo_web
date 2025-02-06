0451d484719a78ed09b3587c626f827e9407b0f9
Paul-A-Lavender <2585827828@qq.com>
2025-02-06 16:02:20 +0800

    fix gitignore typo; fix incompatible trimesh version

--- interfaces.py
+++ interfaces.py
@@ -19,7 +19,7 @@ def voxelize_obj(path:str,output:str="./cache/default.npy")->np.ndarray:
     '''
 
     # 读取 .obj 文件
-    mesh = trimesh.load_mesh(path).to_mesh()
+    mesh = trimesh.load_mesh(path)
     # 目标体素尺寸
     target_shape = (400, 100, 600)
 
